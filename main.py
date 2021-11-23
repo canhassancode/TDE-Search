610888b9386e3f609cea87c587da41bc9ece2d76
Hassan <hassanali25@hotmail.co.uk>
2021-11-23 14:12:31 +0000

    fixed error handling

--- main.py
+++ main.py
@@ -31,32 +31,41 @@ def yaml_files():
                 current_yaml = (os.path.join(root, file)).replace("\\", "/")
                 yaml_list.append(current_yaml)
 
-
-# Opening YAML/YML files and search for attack.t tag
 def find_tag():
     yaml_files()  # obtain all the yaml files available
     counter = 0
     error_counter = 0
     output_list = []
-    for yaml_file in yaml_list:
-        with open(yaml_file, "r") as stream:
-            try:
-                # load the yaml file into a temp variable
-                temp_yaml = yaml.safe_load(stream)
-                # print(temp_yaml)
-                for tag in temp_yaml['tags']:
-                    if tag == "attack." + input_tag:
-                        counter += 1
-                        output_list.append(yaml_file)
-                        # print(counter, ". ",yaml_file)
 
-            except (yaml.YAMLError, KeyError, UnicodeDecodeError) as e:
-                print("ERROR ", error_counter, ": ", e, " | ", yaml_file)
-                error_counter+=1
-                continue
+    for yaml_file in yaml_list:
+        try:
+            stream = open(yaml_file, "r", encoding="utf-8")
+            temp_yaml = yaml.safe_load(stream)
+            for tag in temp_yaml['tags']:
+                if tag == "attack." + input_tag:
+                    counter += 1
+                    output_list.append(yaml_file)
+                    print(counter, ". ",yaml_file)
+        except yaml.YAMLError as x:
+            # print("YAML ERROR: ", x)
+            stream = open(yaml_file, "r")
+            temp_yaml = list(yaml.safe_load_all(stream))
+            for tag in temp_yaml[0]['tags']:
+                if tag == "attack." + input_tag:
+                    counter += 1
+                    output_list.append(yaml_file)
+                    print(counter, ". ",yaml_file)
+            continue
+        except KeyError as y:
+            print("KEY ERROR: ", y)
+            error_counter+=1
+            continue
+        except UnicodeDecodeError as z:
+            print("UNICODE ERROR: ", z, yaml_file)
+            error_counter+=1
+            continue
     
     print("Number of files found: ", counter)  # final output
-
 
 ###########################################
 # RUN CODE
